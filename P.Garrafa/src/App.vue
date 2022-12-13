9ff4187a206a6f9171a64fadc1c3a7ca1e00a3d3
Gustavo P. Gorges <99231805+GustavodePaulaGorges@users.noreply.github.com>
2022-12-13 16:58:47 -0300

    Add files via upload

---
+++ P.Garrafa/src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div class="flex flex-row w-full h-screen items-center justify-center">
+  <div class="bg-white p-4 w-2/5 text-left ">
+    <h1 class="text-8xl underline text-red-500">
+      Crie seu Design Já!
+    </h1>
+  </div>
+  <div class="bg-gray-900 p-4 w-3/5 h-screen" >
+    
+  </div>
+  <h1>a</h1>
+
+</div>
+<h1>a</h1>
+
+</template>
+
---
+++ P.Garrafa/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/App.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
